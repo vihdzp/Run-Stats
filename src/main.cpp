39ef084ec15c6858ea92e0ce8b23bc7670e0da80
Violeta Hernández <vi.hdz.p@gmail.com>
2024-04-26 21:31:25 -0600

    Fix bug

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,14 @@
 using namespace geode::prelude;
 
 /// Represents a section in a level.
-/*struct Section {
+struct Section {
 	/// The name of the section (including the percentage it spans).
-	const char* name;
+	std::string* name;
 	/// Starting percentage.
 	int start;
-};*/
+};
 
+/// Display run stats while playing.
 class RunStatsWidget : public CCNode {
 public:
 	/// Displays the name of the section.
@@ -23,7 +24,7 @@ public:
 	/// Displays the success chance for the entire level.
 	//CCLabelBMFont* m_level_percent = nullptr;
 
-	//Section* sections = nullptr;
+	Section* sections = nullptr;
 	int section_count = 0;
 	int current_section = 0;
 
@@ -40,41 +41,52 @@ public:
 		}
 	}
 
+	/// Initialize widget. 
 	bool init() {
 		if (!CCNode::init()) {
 			return false;
 		}
 
-		//Section sec1 = { "First click (0-2)", 0 };
-		//Section sec2 = { "Everything else (2-100)", 2 };
+		Section sec1 = { new std::string{ "First click (0-2)" }, 0 };
+		Section sec2 = { new std::string{ "Second click (3-4)" }, 3 };
+		Section sec3 = { new std::string{ "Everything else (5-100)" }, 5 };
 
 		// FIXME: where to free this memory?
-		//sections = new Section[]{ sec1, sec2 };
-		section_count = 2;
+		sections = new Section[]{ sec1, sec2, sec3 };
+		section_count = 3;
 
 		const auto anchor = ccp(0.0, 1.0);
 
 		// Dummy text for now. 
-		auto section_name = CCLabelBMFont::create("0% - 1%", "bigFont.fnt");
-		section_name->setOpacity(64);
-		section_name->setAnchorPoint(anchor);
-		section_name->setPosition(0.0, 0.0);
+		m_section_name = CCLabelBMFont::create(sec1.name->c_str(), "bigFont.fnt");
+		m_section_name->setOpacity(64);
+		m_section_name->setAnchorPoint(anchor);
+		m_section_name->setPosition(0.0, 0.0);
 
-		auto label_size = section_name->getScaledContentSize();
+		auto label_size = m_section_name->getScaledContentSize();
 
-		auto section_percent = CCLabelBMFont::create("dummy", "bigFont.fnt");
-		section_percent->setOpacity(64);
-		section_percent->setAnchorPoint(anchor);
-		section_percent->setPosition(0.0, -label_size.height);
+		m_section_percent = CCLabelBMFont::create("dummy", "bigFont.fnt");
+		m_section_percent->setOpacity(64);
+		m_section_percent->setAnchorPoint(anchor);
+		m_section_percent->setPosition(0.0, -label_size.height);
 
-		this->addChild(section_name);
-		this->addChild(section_percent);
+		this->addChild(m_section_name);
+		this->addChild(m_section_percent);
 		this->setContentSize({ label_size.width, 2 * label_size.height });
 
 		return true;
 	}
+
+	/// Clean up memory.
+	~RunStatsWidget() {
+		for (int i = 0; i < section_count; i++) {
+			delete sections[i].name;
+		}
+		delete[] sections;
+	}
 };
 
+/// Add our widget to the play layer.
 class $modify(PlayLayer) {
 	/// m_fields
 	struct Fields {
@@ -100,28 +112,36 @@ class $modify(PlayLayer) {
 		}
 
 		const auto win_size = CCDirector::sharedDirector()->getWinSize();
-
 		auto widget = RunStatsWidget::create();
 		widget->setAnchorPoint(ccp(0.0, 0.0));
 		widget->setPosition(0.0, win_size.height);
 		widget->setScale(0.3);
 		widget->setZOrder(999);
 		this->addChild(widget);
+		m_fields->m_widget = widget;
 
 		return true;
 	}
 
 	/// Resets the section.
-	/*void resetLevel() {
+	void resetLevel() {
 		PlayLayer::resetLevel();
 		auto widget = m_fields->m_widget;
-		widget->m_section_name->setCString("0% - 1%");
-	}*/
+		widget->current_section = 0;
+		widget->m_section_name->setString(widget->sections[0].name->c_str());
+	}
 
 	/// Updates the section.
 	void updateProgressbar() {
 		PlayLayer::updateProgressbar();
 		auto widget = m_fields->m_widget;
-		widget->m_section_name->setCString("1% - 100%");
+		int next_section = widget->current_section + 1;
+		if (next_section < widget->section_count) {
+			auto sec = widget->sections[next_section];
+			if (sec.start <= this->getCurrentPercentInt()) {
+				widget->current_section = next_section;
+				widget->m_section_name->setString(sec.name->c_str());
+			}
+		}
 	}
 };
